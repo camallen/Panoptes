8630ee14f9b58820ec39c1224331ec5b8d02881a
Campbell Allen <campbell.allen@gmail.com>
2020-02-14 17:11:17 +0000

    remove eager load config in dev
    this causes wokrer timeouts in puma

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,9 +5,8 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = ENV.fetch('RAILS_CACHE_CLASSES', false)
-
   # Do not eager load code on boot.
-  config.eager_load = ENV.fetch('RAILS_EAGER_LOAD_CODE', false)
+  config.eager_load = false
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true

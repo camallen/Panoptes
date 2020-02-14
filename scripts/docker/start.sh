ffb669d2344ccfebf0dc752cd6a079e12e4eb2db
Campbell Allen <campbell.allen@gmail.com>
2020-02-14 13:45:45 +0000

    use supervisor and avoid foreman

--- scripts/docker/start.sh
+++ scripts/docker/start.sh
@@ -12,18 +12,15 @@ tmpreaper 7d /tmp/
 mkdir -p tmp/pids/
 rm -f tmp/pids/*.pid
 
-if [ "$RAILS_ENV" == "development" ]; then
-  exec bundle exec foreman start
-else
+if [ "$RAILS_ENV" != "development" ]; then
   if [ ! -d public/assets ]
   then
       bundle exec rake assets:precompile
   fi
-
-  if [ -f "commit_id.txt" ]
-  then
-    cp commit_id.txt public/
-  fi
-
-  exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 fi
+
+if [ -f "commit_id.txt" ]; then
+  cp commit_id.txt public/
+fi
+
+exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf

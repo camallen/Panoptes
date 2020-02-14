8e4a8aa23c3d539b47ca4ff3d4969dd9fb8b8e23
Campbell Allen <campbell.allen@gmail.com>
2020-02-14 14:28:13 +0000

    remove staging logstasher

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -90,9 +90,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # Enable the logstasher logs for the current environment
-  config.logstasher.enabled = true
-  # Enable logging of controller params
-  config.logstasher.log_controller_parameters = true
 end

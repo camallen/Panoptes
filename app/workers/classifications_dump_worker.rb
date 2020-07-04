5fd3f537ea7b9ca21e22d17f577598327071f99e
Campbell Allen <campbell.allen@gmail.com>
2020-07-04 19:58:12 +0100

    add env flag to switch to caching dump

--- app/workers/classifications_dump_worker.rb
+++ app/workers/classifications_dump_worker.rb
@@ -14,27 +14,28 @@ class ClassificationsDumpWorker
     if @resource = CsvDumps::FindsDumpResource.find(resource_type, resource_id)
       @medium = CsvDumps::FindsMedium.new(medium_id, @resource, dump_target).medium
       scope = get_scope(resource)
-  # run this one first
-  @processor = CsvDumps::DumpProcessor.new(formatter, scope, medium)
-
-  # add a feature flag to switch this behaviour on/off at run time (Code experiment? example for tracking data?)
-
-      # @processor = CsvDumps::CachingDumpProcessor.new(formatter, scope, medium) do |formatter|
-      #   # if no pre-formatted cached resource then create a one for re-use in future exports
-      #   unless formatter.cache_resource
-      #     # dump scopes are wrapped in a read replica block by default
-      #     # ensure these writes goto the primary db
-      #     Standby.on_primary do
-      #       classification = formatter.model
-      #       cached_export = CachedExport.create!(
-      #         resource: classification,
-      #         data: ClassificationExport.hash_format(formatter)
-      #       )
-      #       # link the newly saved classification export to the classification for re-use
-      #       classification.update_column(:cached_export_id, cached_export.id) # rubocop:disable Rails/SkipsModelValidations
-      #     end
-      #   end
-      # end
+      if ENV['RUN_CACHING_DUMP']
+        # add a feature flag to switch this behaviour on/off at run time (Code experiment? example for tracking data?)
+        @processor = CsvDumps::CachingDumpProcessor.new(formatter, scope, medium) do |formatter|
+          # if no pre-formatted cached resource then create a one for re-use in future exports
+          unless formatter.cache_resource
+            # dump scopes are wrapped in a read replica block by default
+            # ensure these writes goto the primary db
+            Standby.on_primary do
+              classification = formatter.model
+              cached_export = CachedExport.create!(
+                resource: classification,
+                data: ClassificationExport.hash_format(formatter)
+              )
+              # link the newly saved classification export to the classification for re-use
+              classification.update_column(:cached_export_id, cached_export.id) # rubocop:disable Rails/SkipsModelValidations
+            end
+          end
+        end
+      else
+        # run old dump worker by default
+        @processor = CsvDumps::DumpProcessor.new(formatter, scope, medium)
+      end
 
       @processor.execute
 
